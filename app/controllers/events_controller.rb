5a8e262e76ba8bd3b64eb2cc269b1a1bfafcb77b
lucas-perata <contacto.perata@gmail.com>
2022-05-22 13:37:06 -0300

    Edicion de perfil

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -81,7 +81,7 @@ class EventsController < ApplicationController
     @event.destroy
 
     respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.html { redirect_to root_path, notice: "Event was successfully destroyed." }
       format.json { head :no_content }
     end
   end
